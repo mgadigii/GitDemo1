690b87d1570b379c0966db0aac85a621307e78be
manjunath <mgadigii@yahoo.co.in>
2025-01-19 20:52:14 +0530

    this is commit from GITdemo1

--- src/test/java/Testing/Demo.java
+++ src/test/java/Testing/Demo.java
@@ -2,15 +2,27 @@ package Testing;
 
 import org.testng.annotations.Test;
 
-@Test
+
 
 public class Demo {
 
 	
-	
+	@Test
 	public void run()
 	{
 		System.out.println("hii Everyone");
+	}
+	
+	@Test
+	public void run1()
+	{
+		System.out.println("New println added");
+	}
+	
+	@Test
+	public void run2()
+	{
+		System.out.println("New println added");
 	}
 
 }
