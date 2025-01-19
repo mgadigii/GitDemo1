1fb6de0a043d8bca6df5cf23f4a6a01c961502f2
manjunath <mgadigii@yahoo.co.in>
2025-01-19 21:39:38 +0530

    Commit from GITX

--- src/test/java/Testing/Git_Test_Childbranch.java
+++ src/test/java/Testing/Git_Test_Childbranch.java
@@ -1,10 +1,32 @@
 package Testing;
 
+import org.testng.annotations.Test;
+
 public class Git_Test_Childbranch {
 	
 	@Test
 	public void testSys()
 	{
-		System.out.println("Test ChildBranch");	}
+		System.out.println("Test ChildBranch");	
+		
+	}
+	@Test
+	public void testSys1()
+	{
+		System.out.println("GITX");	
+		
+	}
+	@Test
+	public void testSys2()
+	{
+		System.out.println("GITX");	
+		
+	}
+	@Test
+	public void testSys3()
+	{
+		System.out.println("GITX");	
+		
+	}
 
 }
